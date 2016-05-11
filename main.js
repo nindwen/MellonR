3c2170ea7e54c3137d8d941799b3a5dbbfc52c0c
nindwen <me@nindwen.blue>
2016-05-11 21:11:08 +0300

    Move scripts and styles to their own files

---
+++ main.js
@@ -0,0 +1,24 @@
+var words;
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+            words = JSON.parse(xhttp.responseText).words;
+      }
+};
+xhttp.open("GET", "words.json", true);
+xhttp.send();
+
+
+function generate() {
+      var seed = document.getElementById('seed').value;
+
+      Math.seedrandom(seed);
+      var secret="";
+      for(i=0;i<5;i++) {
+            secret += words[Math.floor(Math.random()*words.length)]+" ";
+      }
+      document.getElementById('secret').innerHTML = secret;
+      hex = Math.round(255-Math.random()*100).toString(16);
+      console.log(hex);
+      document.getElementsByTagName('body')[0].style.background="#"+hex+hex+hex;
+}
